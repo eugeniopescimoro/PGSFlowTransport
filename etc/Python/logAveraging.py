Log-averaging bug fixed

etc/Python/logAveraging.py
@@ -13,7 +13,7 @@ def logAve(ndT, cBoolean, sdc, s, tLS, dcLS, dcLSnorm):
     dcLog = [] # List needed to store the new average concentrations which comes after log-binning
     tThrs = [round(val, 10) for z, val in enumerate(ndT[:-s]) if cBoolean[z]] # it selects the time only if cBoolean is True
     dcThrs = [round(val, 10) for z, val in enumerate(sdc) if cBoolean[z]]
-    logSpacing = np.logspace(np.log10(min(tThrs)), np.log10(max(tThrs)), 150, endpoint=True) # Log-spaced list whose values range between time boundaries 
+    logSpacing = np.logspace(np.log10(min(tThrs)+1e-6), np.log10(max(tThrs)), 150, endpoint=True) # Log-spaced list whose values range between time boundaries 
     logSpacing = np.insert(logSpacing, 0, 0) # it adds 0 at the 0th position
     for k in range(0, len(logSpacing)-1, 1):
         Tlog = [] # Support list: it collects the times within the Kith log-interval so that an average can be performed