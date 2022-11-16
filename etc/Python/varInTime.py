e0005bfa5d2bc6ca3819652d9631b0e5b7a9111f
eugeniopescimoro <eugenio.pescimoro@nottingham.ac.uk>
2022-11-16 17:41:09 +0200

    varInTime for high Peclet numbers

--- etc/Python/varInTime.py
+++ etc/Python/varInTime.py
@@ -11,7 +11,24 @@ import os
 from pathlib import Path
 import subprocess
 
-varInTimeRoot = '/Users/pmxep5/Git/Hub/OpenFOAM/PGSFlowTransport/tutorials/Herten/varPeclet/lowPeclet/'
+# N.B.: varInTime is the output of the postProcess command 
+# "mpirun --oversubscribe -np 96 postProcess -dict system/fieldMetricsDict -field c -parallel >> varInTime 2>&1"
+# run on the server where concentration fields for different time steps are stored and the following functionObject in the system/fieldMetricsDict
+# functions
+# {
+#     concentrationMetrics
+#     {
+#         type        fieldMetrics;
+#         libs        ("libfieldMetricsFunctionObject.so");
+#         operations  ("mean" "meanConc");
+#         nRadii      100;
+#         maxDist     1.0;
+#         fieldName   c;
+#     }
+# }
+# Transfer from the server to local machine with "myscp /data/pmxep5-8/PGSFlowTransport/tutorials/RESULTS/Herten/varPeclet/highPeclet/varInTime ."
+
+varInTimeRoot = '/Users/pmxep5/Git/Hub/OpenFOAM/PGSFlowTransport/tutorials/Herten/varPeclet/highPeclet/'
 varInTimePath = Path(varInTimeRoot)
 os.chdir(varInTimePath)
 os.makedirs("./LOGs", exist_ok = True)
@@ -40,22 +57,34 @@ with open(Path(os.path.join(varInTimeRoot, 'LOGs/VarConcX')), 'r') as Ivar:
         ivar.append(float(line))
 
 y = []
-m = (ivar[2]-ivar[1])/(t[2]-t[1])
+z = []
+a = 1e-6
+b = 2
+# m = (ivar[2]-ivar[1])/(t[2]-t[1])
+# for i in range(len(ivar)):
+#     y.append(m*(t[i]-t[1])+ivar[1])
 for i in range(len(ivar)):
-    y.append(m*(t[i]-t[1])+ivar[1]) 
+    y.append(a*t[i]**b)
 
 font = {'size': 50}
 plt.rc('font', **font)
 plt.figure(figsize=(14, 9))
 plt.grid(True, which="both")
 plt.loglog(t, var, lw=5, color='0.80', label="variance", marker="s", markersize=10)
-plt.loglog(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
-plt.loglog(t, y, lw=5, color='blue', label='ref slope')
+# plt.loglog(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
+# plt.loglog(t, y, lw=5, color='blue', label='ref slope')
 plt.legend(loc="best", fontsize=30)
 
 plt.figure(figsize=(14, 9))
 plt.grid(True, which="both")
-plt.plot(t, var, lw=5, color='0.80', label="variance", marker="s", markersize=10)
-plt.plot(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
-plt.plot(t, y, lw=5, color='blue', label='ref slope')
+# plt.plot(t, var, lw=5, color='0.80', label="variance", marker="s", markersize=10)
+plt.loglog(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
+plt.loglog(t, y, lw=5, color='blue', label='y=a*x^b with a=%f b=%f' % (a, b))
 plt.legend(loc="best", fontsize=30)
+
+# plt.figure(figsize=(14, 9))
+# plt.grid(True, which="both")
+# plt.plot(t, var, lw=5, color='0.80', label="variance", marker="s", markersize=10)
+# plt.plot(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
+# plt.plot(t, y, lw=5, color='blue', label='ref slope')
+# plt.legend(loc="best", fontsize=30)
