da34719d59a8cb8daf327f4c2696deb11ff682b1
eugeniopescimoro <eugenio.pescimoro@nottingham.ac.uk>
2022-11-18 01:12:01 +0200

    Multiple variance in time plotted on same chart - just Peclet now

--- etc/Python/varInTime.py
+++ etc/Python/varInTime.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import os
 from pathlib import Path
 import subprocess
+import numpy as np
 
 # N.B.: varInTime is the output of the postProcess command 
 # "mpirun --oversubscribe -np 96 postProcess -dict system/fieldMetricsDict -field c -parallel >> varInTime 2>&1"
@@ -28,11 +29,14 @@ import subprocess
 # }
 # Transfer from the server to local machine with "myscp /data/pmxep5-8/PGSFlowTransport/tutorials/RESULTS/Herten/varPeclet/highPeclet/varInTime ."
 
+sim = 3
+interval = 1
+t = []
+var = []
+ivar = []
+
+simPath = ['varPeclet/lowPeclet/', 'varPeclet/mediumPeclet/', 'varPeclet/highPeclet/']
 latexFolderPath = Path('/Users/pmxep5/Git/Overleaf/Thesis/')
-varInTimeRoot = '/Users/pmxep5/Git/Hub/OpenFOAM/PGSFlowTransport/tutorials/Herten/varPeclet/highPeclet/'
-varInTimePath = Path(varInTimeRoot)
-os.chdir(varInTimePath)
-os.makedirs("./LOGs", exist_ok = True)
 
 #cat log | grep 'Time ' | cut -d ' ' -f3 > LOGs/VarTimeStep
 subprocess.run(['/bin/bash', '-c', 'cat varInTime | grep \'Time =\' | cut -d\' \' -f3 > LOGs/Time'])
@@ -41,49 +45,64 @@ subprocess.run(['/bin/bash', '-c', 'cat varInTime | grep \'Var=\' | cut -d\'=\' 
 #cat log | grep 'VarConcX=' | cut -d '=' -f4 | cut -d ' ' -f1 > LOGs/VarConcX
 subprocess.run(['/bin/bash', '-c', 'cat varInTime | grep \'VarConcX\' | cut -d\'=\' -f4 | cut -d \' \' -f1 > LOGs/VarConcX'])
 
-t = []
-with open(Path(os.path.join(varInTimeRoot, "LOGs/Time")), 'r') as Time:
-    for line in Time.readlines():
-        t.append(float(line))
-del t[1]
-
-var = []
-with open(Path(os.path.join(varInTimeRoot, 'LOGs/Var')), 'r') as Var:
-    for line in Var.readlines():
-        var.append(float(line))
-        
-ivar = []
-with open(Path(os.path.join(varInTimeRoot, 'LOGs/VarConcX')), 'r') as Ivar:
-    for line in Ivar.readlines():
-        ivar.append(float(line))
+for i in range(0, sim, interval):
+    time = []
+    varInTimePath = os.path.join('/Users/pmxep5/Git/Hub/OpenFOAM/PGSFlowTransport/tutorials/Herten/', simPath[i])
+    os.chdir(Path(varInTimePath))
+    os.makedirs("./LOGs", exist_ok = True)
+    with open(Path(os.path.join(varInTimePath, "LOGs/Time")), 'r') as Time:
+        for line in Time.readlines():
+            time.append(float(line))
+    del time[1]
+    t.append(np.array(time))
+    
+    variance = []
+    with open(Path(os.path.join(varInTimePath, 'LOGs/Var')), 'r') as Var:
+        for line in Var.readlines():
+            variance.append(float(line))
+    var.append(np.array(variance))
+    
+    inertialVar = []
+    with open(Path(os.path.join(varInTimePath, 'LOGs/VarConcX')), 'r') as Ivar:
+        for line in Ivar.readlines():
+            inertialVar.append(float(line))
+    ivar.append(np.array(inertialVar))
 
 y = []
 z = []
 a = 1e-6
 b = 2
-# m = (ivar[2]-ivar[1])/(t[2]-t[1])
-# for i in range(len(ivar)):
-#     y.append(m*(t[i]-t[1])+ivar[1])
-for i in range(len(ivar)):
-    y.append(a*t[i]**b)
+for z in range(len(ivar)):
+    y.append(a*t[z]**b)
 
 font = {'size': 50}
 plt.rc('font', **font)
-plt.figure(figsize=(14, 9))
-plt.grid(True, which="both")
-plt.loglog(t, var, lw=5, color='0.80', label="variance", marker="s", markersize=10)
-# plt.loglog(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
-# plt.loglog(t, y, lw=5, color='blue', label='ref slope')
-plt.legend(loc="best", fontsize=30)
-plt.savefig(os.path.join(latexFolderPath, "images/varInTimePeclet.png"))
+
+lin = ['-', '-.', '--']
+col = ['blue', 'orange', 'green', 'red']
 
 plt.figure(figsize=(14, 9))
 plt.grid(True, which="both")
-# plt.plot(t, var, lw=5, color='0.80', label="variance", marker="s", markersize=10)
-plt.loglog(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
-plt.loglog(t, y, lw=5, color='blue', label='y=a*x^b with a=%f b=%f' % (a, b))
+for j in range(0, sim, interval):
+    plt.loglog(t[j], var[j], ls="%s" % lin[j], lw=5, color="%s" % col[j], label="variance", marker="s", markersize=10)
+    # plt.loglog(t, ivar, lw=5, color='0.40', label='inertial variance', marker="s", markersize=10)
+    # plt.loglog(t, y, lw=5, color='blue', label='ref slope')
 plt.legend(loc="best", fontsize=30)
-plt.savefig(os.path.join(latexFolderPath, "images/inertialVarInTimePeclet.png"))
+# plt.savefig(os.path.join(latexFolderPath, "images/mediumPeViT.png"))
+
+plt.figure(figsize=(14, 9))
+plt.grid(True, which="both")
+# N.M.B. PORCO DEMONIO INIZIALIZZARE CON divar = [[]]*sim E' UNA CAGATA PERCHE' CREA TRE SUBLISTS IDENTICHE !!! 
+divar = [[] for i in range(sim)] 
+for j in range(0, sim, interval):
+    # plt.plot(t, var, lw=5, color='0.80', label="variance", marker="s", markersize=10)
+    plt.loglog(t[j], ivar[j], ls="%s" % lin[j], lw=5, color="%s" % col[j], label='inertial variance', marker="s", markersize=10)
+    for k in range(0, len(ivar[j])-1): # Find the significative range of variances to be plotted (before the plateau)
+        divar[j].append((ivar[j][k+1]-ivar[j][k])/(t[j][k+1]-t[j][k]))
+plt.loglog(t[j], y[j], ls="-", lw=5, color="black", label='y=a*x^b with a=%f b=%f' % (a, b))
+plt.axis([min(min(t, key=min)), 2*10**4, min(min(ivar, key=min)), max(max(ivar, key=max))])
+plt.legend(loc="best", fontsize=30)
+# plt.savefig(os.path.join(latexFolderPath, "images/mediumPeIViT.png"))
 
 # plt.figure(figsize=(14, 9))
 # plt.grid(True, which="both")
